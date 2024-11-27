0503a870c4f45ef3977f38afc3b8bdb8a34ccf04
Marcelo Paulino <marcelo.paulinobh@gmail.com>
2024-11-27 14:31:03 -0300

    Atualizações 27/11/2024

--- myproject/users/urls.py
+++ myproject/users/urls.py
@@ -3,8 +3,11 @@ from .views import UserListView,UserCreateView,UserDetailView
 
 urlpatterns = [
 
-path('/',UserListView.as_view(),name='user-list'),
-path('create/',UserCreateView.as_view(),name='user-create'),
-path('<int:user_id>',UserDetailView.as_view(),name='user-detail')
+path('',UserListView.as_view(),name='users-list'),
+path('create/',UserCreateView.as_view(),name='users-create'),
+path('<int:user_id>/',UserDetailView.as_view(),name='users-detail'),
+path('<int:user_id>/delete/',UserDetailView.as_view(),name='users-delete'),
+path('<int:user_id>/put/',UserDetailView.as_view(),name='users-put')
+
 
 ]
--- myproject/users/views.py
+++ myproject/users/views.py
@@ -6,7 +6,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 import json
 
-
+@method_decorator(csrf_exempt, name='dispatch')
 class UserListView(View): #Listar todos users
     def get(self,request):
         users = list(User.objects.values())
@@ -14,6 +14,7 @@ class UserListView(View): #Listar todos users
 
 
 
+@method_decorator(csrf_exempt, name='dispatch')
 class UserCreateView(View): #Criar novo usuário
     def post(self,request):
         data = json.loads(request.body)
@@ -29,11 +30,12 @@ class UserCreateView(View): #Criar novo usuário
         )
         return JsonResponse({'id':user.id},status = 201)
         
+@method_decorator(csrf_exempt, name='dispatch')
 class UserDetailView(View): # Listar especific user
     def get(self,request,user_id):
         user = get_object_or_404(User,id=user_id)
         data = {
-            'id':user.user_id,
+            'id':user.id,
             'first_name': user.first_name,
             'last_name': user.last_name,
             'date_of_birth': user.date_of_birth,
@@ -41,15 +43,19 @@ class UserDetailView(View): # Listar especific user
             
         }
 
-        return JsonResponse(user,safe=False)
+        return JsonResponse(data,safe=False)
 
 
-    def update(self,request,user_id):
+    def put(self,request,user_id):
         user = get_object_or_404(User,id=user_id)
         data = json.loads(request.body)
+        user.first_name=data.get('first_name',user.first_name)
+        user.last_name_name=data.get('last_name',user.last_name)
+        user.date_of_birth=data.get('date_of_birth',user.date_of_birth)
+        user.enrollment_date=data.get('enrollment_date',user.enrollment_date)
         user.save()
         return JsonResponse({'id':user.id},status=200)
-    
+
     def delete(self,request,user_id):
         user = get_object_or_404(User,id=user_id)
         user.delete()
